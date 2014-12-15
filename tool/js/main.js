a259d858b3789140292e78e66100dec12a2d9d08
Mackan90096 <Mackan90096@users.noreply.github.com>
2014-12-15 10:02:42 +0100

    Update main.js

--- tool/js/main.js
+++ tool/js/main.js
@@ -1,7 +1,11 @@
 $(document).ready(function(){
   var machines = $.getJSON('./js/obj.json');
-  
   var appWin = $('#app');
+  var menu = $('#menu');
+  
+  console.log(machines);
+  
+  //menu.append("<div class='mach' id="+machines)
   
   $('.mach').on('click',function(){
     p = appWin.append("<div class='item "+this.id+"'></div>");
