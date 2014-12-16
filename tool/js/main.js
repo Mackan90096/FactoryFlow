dc734f53d2b8d1142a8a243af985223c43a5d387
Mackan90096 <Mackan90096@users.noreply.github.com>
2014-12-16 10:44:06 +0100

    Update main.js

--- tool/js/main.js
+++ tool/js/main.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
   
   $('body').on('keyup', function(e){
     if(e.keyCode == 82){
-      var el = document.getElementsByClassName("selected");
+      var el = document.getElementsByClassName('selected');
 var st = window.getComputedStyle(el, null);
 var tr = st.getPropertyValue("-webkit-transform") ||
          st.getPropertyValue("-moz-transform") ||
