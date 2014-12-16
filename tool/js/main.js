20cf964027fcc3b3086ad741a397d563273b186f
Mackan90096 <Mackan90096@users.noreply.github.com>
2014-12-16 10:40:34 +0100

    Update main.js

--- tool/js/main.js
+++ tool/js/main.js
@@ -34,7 +34,24 @@ $(document).ready(function(){
   
   $('body').on('keyup', function(e){
     if(e.keyCode == 82){
-      var deg = $('.selected').css('transform');
+      var tr = $('.selected').css('transform');
+      var values = tr.split('(')[1];
+    values = values.split(')')[0];
+    values = values.split(',');
+    var a = values[0];
+    var b = values[1];
+    var c = values[2];
+    var d = values[3];
+    
+    var scale = Math.sqrt(a*a + b*b);
+    
+    // arc sin, convert from radians to degrees, round
+    // DO NOT USE: see update below
+    var sin = b/scale;
+    var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+    
+    // works!
+    console.log('Rotate: ' + angle + 'deg');
       console.log(deg);
       $('.selected').rotate(+90);
     }
