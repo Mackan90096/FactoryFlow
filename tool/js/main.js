b5adff8b6c8b29cba6779c9995c3f5f70741a2ec
Mackan90096 <Mackan90096@users.noreply.github.com>
2014-12-16 10:15:19 +0100

    Update main.js

--- tool/js/main.js
+++ tool/js/main.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
     });
   
     $('body').on('click', '.mach', function(){
-       appWin.append("<div class='item "+this.id+"'></div>");
+       appWin.append("<div class='item "+this.id+"'></div>").position({my:'left top',at:'#app'});
         $('.item').draggable({ grid:[ 16, 16 ], containment:'#app' });
   });
   
